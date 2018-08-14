77077c17e435842353624559b1a8c4e731594202
shihaoran <shhr_2006@126.com>
2018-08-15 00:37:05 +0800

    2018.08.15

---
+++ lc_622.py
@@ -0,0 +1,83 @@
+class Node:
+    def __init__(self, value):
+        self.val = value
+        self.next = self.pre = None
+
+
+class MyCircularQueue(object):
+
+    def __init__(self, k):
+        """
+        Initialize your data structure here. Set the size of the queue to be k.
+        :type k: int
+        """
+        self.size = k
+        self.curSize = 0
+        self.head = self.tail = Node(-1)
+        self.head.next = self.tail
+        self.tail.pre = self.head
+
+    def enQueue(self, value):
+        """
+        Insert an element into the circular queue. Return true if the operation is successful.
+        :type value: int
+        :rtype: bool
+        """
+        if self.curSize < self.size:
+            node = Node(value)
+            node.pre = self.tail.pre
+            node.next = self.tail
+            node.pre.next = node.next.pre = node
+            self.curSize += 1
+            return True
+        return False
+
+    def deQueue(self):
+        """
+        Delete an element from the circular queue. Return true if the operation is successful.
+        :rtype: bool
+        """
+        if self.curSize > 0:
+            node = self.head.next
+            node.pre.next = node.next
+            node.next.pre = node.pre
+            self.curSize -= 1
+            return True
+        return False
+
+    def Front(self):
+        """
+        Get the front item from the queue.
+        :rtype: int
+        """
+        return self.head.next.val
+
+    def Rear(self):
+        """
+        Get the last item from the queue.
+        :rtype: int
+        """
+        return self.tail.pre.val
+
+    def isEmpty(self):
+        """
+        Checks whether the circular queue is empty or not.
+        :rtype: bool
+        """
+        return self.curSize == 0
+
+    def isFull(self):
+        """
+        Checks whether the circular queue is full or not.
+        :rtype: bool
+        """
+        return self.curSize == self.size
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
--- lc_670.py
+++ lc_670.py
@@ -5,30 +5,23 @@ class Solution(object):
         :rtype: int
         """
         s = str(num)
-        l = []
-        prev = 10
-        idx_f = -1
-        idx_s = -1
+        l = [-1]*10
+        s_list = list(s)
         for idx, c in enumerate(s):
             c = int(c)
-            if c > prev:
-                for i, v in enumerate(l):
-                    if v < c:
-                        idx_f = i
-                        idx_s = idx
-                        break
-                break
-            else:
-                l.append(c)
-                prev = c
-        if idx_s != -1:
-            str_list = list(s)
-            temp = str_list[idx_s]
-            str_list[idx_s] = str_list[idx_f]
-            str_list[idx_f] = temp
-            s = ''.join(str_list)
+            l[c] = idx
+        for idx, c in enumerate(s):
+            c = int(c)
+            for i in range(9,c,-1):
+                if l[i] > idx:
+                    temp = s_list[l[i]]
+                    s_list[l[i]] = s_list[idx]
+                    s_list[idx] = temp
+                    return int(''.join(s_list))
+        return int(''.join(s_list))
 
-        return int(s)
+
+
 
 
 
@@ -36,4 +29,4 @@ if __name__ == '__main__':
     # begin
     s = Solution()
 
-    print s.maximumSwap(9973)
+    print s.maximumSwap(98368)
